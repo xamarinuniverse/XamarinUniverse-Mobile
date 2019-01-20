8d35366a157a98933babdfd7b25016bb826b2cbc
luismts <deivi_0326@hotmail.com>
2019-01-20 14:15:27 -0400

    Base Xamarin Universe Mobile added

---
+++ Source/XamarinUniverse.Mobile/XamarinUniverse.Mobile/XamarinUniverse.Mobile/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinUniverse.Mobile.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamarinUniverse.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
